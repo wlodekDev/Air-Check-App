bae53d598eca16f064f8aa2afcd9fdda24248652
Wlodek <w4d.dev@gmail.com>
2018-04-27 15:27:27 +0200

    http changed to https in getGeolocationData.js

--- src/js/api/getGeolocationData.js
+++ src/js/api/getGeolocationData.js
@@ -1,5 +1,5 @@
 function getAPI(lat, long, callback) {
-    let url = 'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&APPID=65abbbf90e06e64ea43e8b0ebaa43bb0';
+    let url = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&APPID=65abbbf90e06e64ea43e8b0ebaa43bb0';
     fetch(url)
         .then(res => {
             if (res.ok){
