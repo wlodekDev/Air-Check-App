77c88b99c82c43f7afb8e1efe74db65db88ccd14
Wlodek <w4d.dev@gmail.com>
2018-04-27 15:04:37 +0200

    minor changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: "./src/js/app.js",
     output: {  path: path.join(__dirname, "./dist/"), filename : 'out.js' },
     watch: true,
