958d0dd04c24722ca829ed4c06669603cecfdc56
Wlodek <w4d.dev@gmail.com>
2018-04-29 19:25:56 +0200

    minor changes

--- src/js/components/mainComponent.jsx
+++ src/js/components/mainComponent.jsx
@@ -13,7 +13,7 @@ class MainWrapper extends React.Component {
         const lon = this.props.lon;
 
         return <div className='mainWrapper'>
-            <div>
+            <div className='ribbon'>
                 <a href="https://github.com/wlodekDev" target='_blank'><img style={{position: 'absolute', top: '0', right: '0', border: '0'}} src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"/></a>
             </div>
             <LocationModule lat={lat} lon={lon}/>
