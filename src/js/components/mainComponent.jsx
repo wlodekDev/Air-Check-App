a1b94ff7c6b3e1cfd47110100149ff1c08de37b0
Wlodek <w4d.dev@gmail.com>
2018-04-29 19:30:30 +0200

    minor changes

--- src/js/components/mainComponent.jsx
+++ src/js/components/mainComponent.jsx
@@ -14,7 +14,7 @@ class MainWrapper extends React.Component {
 
         return <div className='mainWrapper'>
             <div className='ribbon'>
-                <a href="https://github.com/wlodekDev" target='_blank'><img style={{position: 'absolute', top: '0', right: '0', border: '0'}} src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"/></a>
+                <a href="https://github.com/wlodekDev/wlodekDev.github.io" target='_blank'><img style={{position: 'absolute', top: '0', right: '0', border: '0'}} src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"/></a>
             </div>
             <LocationModule lat={lat} lon={lon}/>
             <TempModule lat={lat} lon={lon} />
