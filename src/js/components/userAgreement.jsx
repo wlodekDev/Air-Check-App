39fff077394e6d00bf91b42bcac75bb79fe0a4dd
Wlodek <w4d.dev@gmail.com>
2018-04-29 10:20:57 +0200

    divInfo font size changed

--- src/js/components/userAgreement.jsx
+++ src/js/components/userAgreement.jsx
@@ -100,7 +100,6 @@ class AgreementModule extends React.Component {
             </div>
         } if(this.state.userAgreement !== null) {
             return <div>
-                {/*<a href="https://github.com/wlodekDev"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"/></a>*/}
                 <div style={style}>
                     <MainWrapper lat={this.state.lat} lon={this.state.lon} />
                 </div>
